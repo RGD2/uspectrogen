b41bae45533c9726b2d264ba2a6ef15ea9503109
RGD2 <remydyer@gmail.com>
2023-03-22 11:22:11 +1100

    fixed send.py

--- send.py
+++ send.py
@@ -2,7 +2,10 @@ import serial
 from sys import argv
 
 try: 
-    with Serial.serial('/dev/ttyAMA0', 2500000) as ser:
-        ser.write(argv[1])
+    with serial.Serial('/dev/ttyAMA0', 2500000) as ser:
+        ser.write(bytes(argv[1], encoding='utf-8'))
+
+except (KeyboardInterrupt):
+    pass
 
 
